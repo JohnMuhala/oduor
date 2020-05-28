7da66c143d8868c7fc9c070e053d9016283d7400
John Oduor <oduorjohn98@gmail.com>
2020-05-28 23:30:23 +0300

    my capitalized function

---
+++ capitalized.js
@@ -0,0 +1,15 @@
+//...Direction
+//write a function that accept a string the function should
+// capitalize the fast letter of each word in the string then return the capitalized string
+// ....Example
+// capitalized('a short sentence') --> 'a shirt sentence'
+//capitalize('a laze=y fox')  --> 'a lazy fox'
+// capitalize('look,it is working!')  ---> 'look it is working'
+
+let string = 'book'
+function capitalize (str){
+    const my_string = [],
+    let string =str.split('').tostring()
+}
+return capitalize(string);
+console.log(string)
