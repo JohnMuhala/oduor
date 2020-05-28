55011927809659db1c77fc2d8931f789325a8bc0
John Oduor <oduorjohn98@gmail.com>
2020-05-28 23:13:49 +0300

    My Anagram Function

---
+++ anagrams.js
@@ -0,0 +1,24 @@
+//--directions
+// check to see if two provided strings are anagrams ore eachother.
+//one string is an anagram of anlother if it uses the same characters
+//if the same quality.Only consider characters not spaces 
+//---Examples 
+ //  anagrams ('raile safety', 'fairy tales',)--> true
+
+
+ let stringA = 'rail safety'
+ let stringB = 'fairy tales'
+
+ function anagrams(stringA, stringB){
+     let strA = my_string(stringA === stringB)
+     let strB = my_string(stringB === StringA)
+
+     if(stringA ===stringB)
+     {console.log('true')}
+     else if{
+         console.log('true')
+
+     }else{
+         console.log('false')
+     }
+ }
